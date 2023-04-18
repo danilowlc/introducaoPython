e538b6ba392261e105afad4164850fd7dd3de551
danilowlc <danilowlc@gmail.com>
2023-04-17 22:13:55 -0300

    estudando...

--- firstcode.py
+++ firstcode.py
@@ -1,5 +1,20 @@
 print("Hello World")
 
 #variaveis e tipos de dados
+# -> int (inteiro)
+# -> float (decimal)
+# -> str (string)
+# -> boll (Boleano)
+
 idade = 21
-print(idade)
+valor = 21.1
+nome = "Danilo"
+estudando_python = True
+
+#Entrada e Saida de Dados
+print(f'Sua idade é: {idade}')
+print(f'Seu nome é: {nome}')
+print(f'Ola, {nome} sua idade é {idade}')
+print(34*'-')
+nova_idade = input("digite sua idade: \n")
+print(f'A idade é {nova_idade}')
